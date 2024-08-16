260fefeef179f30c0b557c1bb8146f76430937e1
venkikgit <venki.apiiit@gmail.com>
2024-08-16 09:02:32 +0530

    fix formatting errors and create interface for email functinality

---
+++ helloWorld.ts
@@ -0,0 +1,119 @@
+/* eslint-disable no-console */
+import axios from 'axios';
+import readline from 'node:readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function promptInput(inputName) {
+    return new Promise((resolve) => {
+        rl.question(
+            `Enter the ${inputName} to send a welcome message: `,
+            (item) => {
+                resolve(item);
+            },
+        );
+    });
+}
+async function sendEmailWithQueue(email, subject, text) {
+    try {
+        const response = await axios.post(
+            'http://localhost:4000/api/notifications/email/v1/queue-email',
+            {
+                to: email,
+                subject: subject,
+                text: text,
+            },
+        );
+
+        console.log('Email queued successfully!');
+        console.log('Job ID:' + JSON.stringify(response.data));
+        return response.data;
+    } catch (error) {
+        console.error('Failed to queue email:', error.message);
+    }
+}
+
+async function sendEmailWithBaiscFunctinality(email, subject, text) {
+    try {
+        const response = await axios.post(
+            'http://localhost:4000/api/notifications/email/v1/send-email',
+            {
+                to: email,
+                subject: subject,
+                text: text,
+            },
+        );
+
+        console.log('Email sent successfully!');
+        console.log('Job ID:' + JSON.stringify(response.data));
+        return response.data;
+    } catch (error) {
+        console.error('Failed to send email:', error.message);
+    }
+}
+async function getEmailJobIdStatus(jobId) {
+    try {
+        const response = await axios.get(
+            `http://localhost:4000/api/notifications/email/v1/email-status/${jobId}`,
+        );
+
+        console.log('Job Status fetched successfully!');
+        console.log('Job ID:' + JSON.stringify(response.data));
+        return response.data;
+    } catch (error) {
+        console.error('Failed to get email status', error.message);
+    }
+}
+
+async function main() {
+    while (true) {
+        console.info('\n1. Send email with basic functionality');
+        console.info('2. Send email with retry functionality');
+        console.info('3. Check Job ID status for retry functionality');
+        console.info('4. Exit');
+
+        const choice = await new Promise((resolve) => {
+            rl.question('Choose an option (1-4): ', resolve);
+        });
+
+        switch (choice) {
+            case '1': {
+                const email = await promptInput('email');
+                const subject = await promptInput('subject');
+                const text = await promptInput('text');
+                console.log('entered email' + email);
+                if (email && subject && text) {
+                    await sendEmailWithBaiscFunctinality(email, subject, text);
+                }
+                break;
+            }
+            case '2': {
+                const email = await promptInput('email');
+                const subject = await promptInput('subject');
+                const text = await promptInput('text');
+                console.log('entered email' + email);
+                if (email && subject && text) {
+                    await sendEmailWithQueue(email, subject, text);
+                }
+                break;
+            }
+            case '3': {
+                const jobId = await promptInput('JobId');
+                if (jobId) {
+                    await getEmailJobIdStatus(jobId);
+                }
+                break;
+            }
+            case '4':
+                rl.close();
+                return;
+            default:
+                console.log('Invalid option. Please try again.');
+        }
+    }
+}
+
+main();
--- worker.ts
+++ worker.ts
@@ -1,7 +1,7 @@
+/* eslint-disable no-console */
 import queueManagerService from './src/services/queueManagerService.ts';
 import { Worker } from 'bullmq';
 import emailService from './src/services/emailService.ts';
-import { logger } from './src/utils/logger.ts';
 import fs from 'fs';
 
 const emailQueue = queueManagerService.emailQueue;
@@ -11,7 +11,7 @@ const worker = new Worker(
     'email-queue',
     async (job) => {
         const { to, subject, text } = job.data;
-        logger.debug(job.opts.attempts);
+        console.log(job.opts.attempts);
         let attempts = job.opts.attempts || 0;
         // if (job.data.attempts) {
         //     attempts = job.data.attempts;
@@ -23,12 +23,12 @@ const worker = new Worker(
             }
             // throw new Error('Email send failed');
             fs.writeFileSync('email-success.log', JSON.stringify(job));
-            logger.info(
+            console.info(
                 `Email sent to ${to} with subject "${subject}" with ${job.id} using primary email service`,
             );
         } catch (err: Error | unknown) {
-            logger.error('Error sending email:', err);
-            logger.info('Job data:' + JSON.stringify(job.data));
+            console.error('Error sending email:', err);
+            console.info('Job data:' + JSON.stringify(job.data));
 
             if (attempts < 3) {
                 attempts++;
@@ -45,7 +45,7 @@ const worker = new Worker(
                     },
                 );
             } else {
-                logger.warn(
+                console.warn(
                     'Maximum retry attempts reached for job . Trying one more time using Secondary Email Service:',
                     job.id,
                 );
@@ -55,11 +55,11 @@ const worker = new Worker(
                     if (job.id) {
                         await queueManagerService.emailQueue.remove(job.id);
                     }
-                    logger.info(
+                    console.info(
                         `Email sent to ${to} with subject "${subject}" with ${job.id} using secondary email service`,
                     );
                 } catch (error) {
-                    logger.error(
+                    console.error(
                         'Error Occured with Senodary Email Service Also' +
                             error,
                     );
@@ -77,4 +77,4 @@ const worker = new Worker(
     },
 );
 
-logger.info('Email worker is running');
+console.log('Email worker is running');
